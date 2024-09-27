8fccf1331126a1b58a92abd90e06b6b38e09c742
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-27 17:24:44 -0400

    RECOMPILADO 24-09-27 17:24

--- servicio-apis/src/main/java/com/galileo/cu/servicioapis/ServicioApisApplication.java
+++ servicio-apis/src/main/java/com/galileo/cu/servicioapis/ServicioApisApplication.java
@@ -22,7 +22,7 @@ public class ServicioApisApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("APIS V-2408280502");
+        System.out.println("APIS V-2408280502 RECOMPILADO 24-09-27 17:24");
     }
 
 }
