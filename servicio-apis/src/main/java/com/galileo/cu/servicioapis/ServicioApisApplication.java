1f72846edbb0725425a42d1abb9a392ee33aff72
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-28 05:31:05 -0400

    recompilando con commons antesbalizasModelosBalizas

--- servicio-apis/src/main/java/com/galileo/cu/servicioapis/ServicioApisApplication.java
+++ servicio-apis/src/main/java/com/galileo/cu/servicioapis/ServicioApisApplication.java
@@ -22,7 +22,7 @@ public class ServicioApisApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("APIS V-2408280502 RECOMPILADO 24-09-28 05:21");
+        System.out.println("APIS V-2408280502 RECOMPILADO 24-09-28 05:30");
     }
 
 }
