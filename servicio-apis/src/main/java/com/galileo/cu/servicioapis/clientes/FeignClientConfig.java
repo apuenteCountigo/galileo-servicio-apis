6b6090a26c22a99e21a86e632df6d2795f66f68f
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-14 05:26:15 -0500

    2- Probando keystore.jks FeignClientConfig

---
+++ servicio-apis/src/main/java/com/galileo/cu/servicioapis/clientes/FeignClientConfig.java
@@ -0,0 +1,34 @@
+package com.galileo.cu.servicioapis.clientes;
+
+import feign.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
+@Configuration
+public class FeignClientConfig {
+
+    @Bean
+    public Client feignClient() throws Exception {
+        // Cargar el truststore
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        try (FileInputStream trustStoreStream = new FileInputStream("/app/resources/keystore.jks")) {
+            trustStore.load(trustStoreStream, "GalileoTraccar".toCharArray());
+        }
+
+        // Inicializar TrustManager
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory
+                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(trustStore);
+
+        // Crear SSLContext
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), new java.security.SecureRandom());
+
+        // Crear Feign Client con SSLContext
+        return new Client.Default(sslContext.getSocketFactory(), (hostname, session) -> true);
+    }
+}
