a0b81b69df67ebac491f9565e2dcc2aad85a1d27
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-12-24 10:49:49 -0500

    Change OF PostMapping TO GetMapping IN estadoServerTraccar

--- servicio-apis/src/main/java/com/galileo/cu/servicioapis/controladores/ApiControlador.java
+++ servicio-apis/src/main/java/com/galileo/cu/servicioapis/controladores/ApiControlador.java
@@ -2792,7 +2792,7 @@ public class ApiControlador {
         String uriBuild = (!Strings.isNullOrEmpty(ipHost)
                 && (!ipHost.contains("http://") && !ipHost.contains("https://")) ? "http://" : "") + ipHost
                 + (!Strings.isNullOrEmpty(puerto) ? ":" + puerto : "");
-        log.info("uriBuild:= {}", uriBuild);
+        // log.info("uriBuild:= {}", uriBuild);
 
         URI uri = null;
         try {
@@ -2812,7 +2812,8 @@ public class ApiControlador {
         String password = conexiones.getPassword();
         byte[] encodedBytes = Base64Utils.encode((username + ":" + password).getBytes());
         String authHeader = "Basic " + new String(encodedBytes);
-        log.info("@@@@@@@ obtenerAutorizacionTraccar: Usu: {}, Pass: {}", username, password);
+        // log.info("@@@@@@@ obtenerAutorizacionTraccar: Usu: {}, Pass: {}", username,
+        // password);
         return authHeader;
     }
 
