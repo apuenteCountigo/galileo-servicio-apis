635f3c30594ea1f4759f0cdcf599edf113f424f3
apuentes+1-at-178934116267 <apuente@soaint.com>
2025-02-13 10:24:10 -0500

    VERSION 1.1.6

--- servicio-apis/src/main/java/com/galileo/cu/servicioapis/ServicioApisApplication.java
+++ servicio-apis/src/main/java/com/galileo/cu/servicioapis/ServicioApisApplication.java
@@ -22,7 +22,7 @@ public class ServicioApisApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("APIS V-1.1.5 25-01-17 11:03");
+        System.out.println("APIS V-1.1.6 25-02-13 10:23");
     }
 
 }
